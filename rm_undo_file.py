8efefe6b4467f9111a78fbb5ee6c391f5f70962c
eeeXun <sdes96303@gmail.com>
2020-11-04 14:40:14 +0800

    rm_undo_file to script

--- rm_undo_file.py
+++ rm_undo_file.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
 import os
 
-undo_dir='/home/xun/.vim/undodir/'
-undo_file_list=os.listdir(undo_dir)
+def main():
+    undo_dir='/home/xun/.vim/undodir/'
+    undo_file_list=os.listdir(undo_dir)
 
-print('Delete undo file:')
-for undo_file in undo_file_list:
-    real_file='/'.join(undo_file.split('%'))
-    if not os.path.exists(real_file):
-        print(undo_dir+undo_file)
-        os.remove(undo_dir+undo_file)
+    print('Delete undo file:')
+    for undo_file in undo_file_list:
+        real_file='/'.join(undo_file.split('%'))
+        if not os.path.exists(real_file):
+            print(undo_dir+undo_file)
+            os.remove(undo_dir+undo_file)
+
+if __name__=='__main__':
+    main()
