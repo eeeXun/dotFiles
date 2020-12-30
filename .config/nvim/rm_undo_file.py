11afe6244137fc5dcce56ed7944cb9792b8903d2
eeeXun <sdes96303@gmail.com>
2020-12-31 00:05:26 +0800

    rm undo file with the message of total volume

--- .config/nvim/rm_undo_file.py
+++ .config/nvim/rm_undo_file.py
@@ -4,18 +4,22 @@ import os
 def main():
     undo_dir='/home/xun/.config/nvim/undodir/'
     undo_file_list=os.listdir(undo_dir)
-    total=0
+    total_file=0
+    total_volume=0
     print('Delete undo file:')
     for undo_file in undo_file_list:
         real_file='/'.join(undo_file.split('%'))
         if not os.path.exists(real_file):
             undo_path=undo_dir+undo_file
+            undo_volume=os.stat(undo_path).st_size
+            total_file+=1
+            total_volume+=undo_volume
             print('=>',end=' ')
-            print('({:.2f}k)'.format(os.stat(undo_path).st_size/1024),end=' ')
+            print('({:.2f}k)'.format(undo_volume/1024),end=' ')
             print(undo_file)
             os.remove(undo_path)
-            total+=1
-    print('Total: {}'.format(total))
+    print('Total volume: ({:.2f}k)'.format(total_volume/1024))
+    print('Total files: {}'.format(total_file))
 
 if __name__=='__main__':
     main()
