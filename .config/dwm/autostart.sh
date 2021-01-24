ed3e9f982701fe9b17c477c2d9cb79e639adfc7d
eeeXun <sdes96303@gmail.com>
2021-01-24 11:48:12 +0800

    Update kill ibus

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-killall ibus-daemon &
-ibus-daemon -d &
+if [ $(( $(ps aux | grep ibus-daemon | wc -l) )) -gt 1 ];then
+    killall ibus-daemon
+fi
 dwmblocks &
+ibus-daemon -d &
 mate-power-manager &
 nm-applet &
 feh --bg-scale ~/Pictures/background3.jpg &
