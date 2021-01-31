2b5b2c6b9af985ce788241ed70456f32e24aecb7
eeeXun <sdes96303@gmail.com>
2021-01-31 23:06:08 +0800

    shell with case

--- setFile.sh
+++ setFile.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-echo "Setting dwm?"
-while read reply;do
-    if [ ${reply:0:1} == "n" ];then
-        break
-    elif [ ${reply:0:1} == "y" ];then
-        sudo cp ./systemSetting/dwm.desktop /usr/share/xsessions/
-        cp -r ./.config/dwm/ ./.config/dwmblocks/ ${HOME}/.config/
-        break
-    else
-        echo "Setting dwm?"
-    fi
+while read -p "Setting dwm? [Y/n] " reply;do
+    case ${reply} in
+        [Nn]*) break;;
+        [Yy]*)
+            sudo cp ./systemSetting/dwm.desktop /usr/share/xsessions/
+            cp -r ./.config/dwm/ ./.config/dwmblocks/ ${HOME}/.config/
+            break;;
+    esac
 done
 
-echo "Setting bspwm?"
-while read reply;do
-    if [ ${reply:0:1} == "n" ];then
-        break
-    elif [ ${reply:0:1} == "y" ];then
-        cp -r ./.config/bspwm/ ./.config/sxhkd/ ./.config/polybar ${HOME}/.config/
-        break
-    else
-        echo "Setting bspwm?"
-    fi
+while read -p "Setting bspwm? [Y/n] " reply;do
+    case ${reply} in
+        [Nn]*) break;;
+        [Yy]*)
+            cp -r ./.config/bspwm/ ./.config/sxhkd/ ./.config/polybar ${HOME}/.config/
+            break;;
+    esac
 done
 
-echo "Setting neovim and apparence?"
-while read reply;do
-    if [ ${reply:0:1} == "n" ];then
-        break
-    elif [ ${reply:0:1} == "y" ];then
-        cp -r ./.config/nvim/ ./.config/mypy/ ./.config/lf/ ${HOME}/.config/
-        cp ./.config/gtk-3.0/settings.ini ${HOME}/.config/gtk-3.0/
-        sudo cp ./systemSetting/01-touchpad.conf /etc/X11/xorg.conf.d/
-        break
-    else
-        echo "Setting bspwm?"
-    fi
+while read -p "Setting neovim and apparence? [Y/n] " reply;do
+    case ${reply} in
+        [Nn]*) break;;
+        [Yy]*)
+            cp -r ./.config/nvim/ ./.config/mypy/ ./.config/lf/ ${HOME}/.config/
+            cp ./.config/gtk-3.0/settings.ini ${HOME}/.config/gtk-3.0/
+            sudo cp ./systemSetting/01-touchpad.conf /etc/X11/xorg.conf.d/
+            break;;
+    esac
 done
