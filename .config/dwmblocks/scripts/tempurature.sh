50d612c488ead272e342a8c49f0383871d5053db
eeeXun <sdes96303@gmail.com>
2021-03-23 02:09:08 +0800

    Change dwmblocks tempurature path

--- .config/dwmblocks/scripts/tempurature.sh
+++ .config/dwmblocks/scripts/tempurature.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-scan_dir=$(ls /sys/devices/platform/coretemp.0/hwmon/)
-tempurature=$(cat /sys/devices/platform/coretemp.0/hwmon/${scan_dir}/temp5_input)
+tempurature=$(cat /sys/class/thermal/thermal_zone0/temp)
 
 echo $((tempurature/1000))°C
