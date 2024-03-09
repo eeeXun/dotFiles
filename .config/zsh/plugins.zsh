72499626e9f25db52cb5455c3e4d9f352819385f
eeeXun <sdes96303@gmail.com>
2024-03-09 17:01:00 +0800

    replace autojump with zoxide

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,5 +1,5 @@
-# autojump
-source "/usr/share/autojump/autojump.zsh"
+# zoxide
+eval "$(zoxide init zsh --cmd j)"
 
 # history-substring-search
 source "/usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
@@ -8,8 +8,6 @@ source "/usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substrin
 source "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # hstr
-setopt histignorespace          # skip cmds w/ leading space from history
-export HSTR_CONFIG="hicolor"    # get more colors
 hstr_no_tiocsti() {
     zle -I
     { HSTR_OUT="$( { </dev/tty hstr ${BUFFER}; } 2>&1 1>&3 3>&- )"; } 3>&1;
@@ -18,4 +16,3 @@ hstr_no_tiocsti() {
     zle redisplay
 }
 zle -N hstr_no_tiocsti
-export HSTR_TIOCSTI=n
--- .zshrc
+++ .zshrc
@@ -1,9 +1,9 @@
 # theme
 source "/usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme"
 # zsh config
-source "$HOME/.config/zsh/plugins.zsh"
+source "$HOME/.config/zsh/env.zsh"
 source "$HOME/.config/zsh/title.zsh"
-source "$HOME/.config/zsh/history.zsh"
 source "$HOME/.config/zsh/alias.zsh"
 source "$HOME/.config/zsh/completion.zsh"
+source "$HOME/.config/zsh/plugins.zsh"
 source "$HOME/.config/zsh/keymaps.zsh"
